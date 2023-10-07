277501a33d2b320810d4e19e2f8ac795bfee765e
drexdev <andrelps.amorim17@gmail.com>
2023-10-07 15:49:32 -0300

    Estilização do site

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 
+import './styles/config.css'
+import './styles/globals.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
